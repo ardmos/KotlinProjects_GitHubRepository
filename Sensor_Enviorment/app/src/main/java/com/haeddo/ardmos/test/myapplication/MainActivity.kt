3962f876b0014fc92a620d8f9f4f09dc7a20f9c7
ardmos <46891528+ardmos@users.noreply.github.com>
2021-12-17 01:31:38 +0900

    기압 센서 사용 기능 추가

--- Sensor_Enviorment/app/src/main/java/com/haeddo/ardmos/test/myapplication/MainActivity.kt
+++ Sensor_Enviorment/app/src/main/java/com/haeddo/ardmos/test/myapplication/MainActivity.kt
@@ -1,25 +1,54 @@
 package com.haeddo.ardmos.test.myapplication
 
+import android.app.Activity
 import android.content.Context
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-private lateinit var sensor_manager : SensorManager
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), SensorEventListener  {
+
+
+    private lateinit var sensorManager: SensorManager
+    private var pressure: Sensor? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sensor_manager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        val device_sensors : List<Sensor> = sensor_manager.getSensorList(Sensor.TYPE_ALL)
+        // Get an instance of the sensor service, and use that to get an instance of
+        // a particular sensor.
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+    }
 
-        for(s in device_sensors){
-            Log.d("MySensors : ", s.name)
-        }
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+        // Do something here if sensor accuracy changes.
+    }
 
+    override fun onSensorChanged(event: SensorEvent) {
+        val millibarsOfPressure = event.values[0]
+        // Do something with this sensor data.
+        Log.d("millibarsOfPressure ", millibarsOfPressure.toString())
+    }
+
+    override fun onResume() {
+        // Register a listener for the sensor.
+        super.onResume()
+        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }
+
+
