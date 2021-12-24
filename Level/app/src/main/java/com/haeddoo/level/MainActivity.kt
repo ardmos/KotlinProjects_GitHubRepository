a5b4a6593e963eee051e632415f952e634a86eb8
ardmos <46891528+ardmos@users.noreply.github.com>
2021-12-24 23:11:20 +0900

    수평계 Google AdMob Adaptive Banner 추가 버전.  여기에 인터넷 권한 신청 필요한가???!!?  일단 출시 검토 신청 버전. v0.2

--- Level/app/src/main/java/com/haeddoo/level/MainActivity.kt
+++ Level/app/src/main/java/com/haeddoo/level/MainActivity.kt
@@ -11,13 +11,16 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import android.widget.Toast
 import com.google.ads.mediation.admob.AdMobAdapter
 import com.google.android.gms.ads.*
 import com.haeddoo.level.databinding.ActivityMainBinding
+import java.util.*
 import kotlin.math.abs
 
+/** Main Activity. Inflates main activity xml and child fragments.  */
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     val vbinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -30,6 +33,26 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     //Google AdMob
     lateinit var ad_view: AdView
+    private var initialLayoutComplete = false
+    // Determine the screen width (less decorations) to use for the ad width.
+    // If the ad hasn't been laid out, default to the full screen width.
+
+    private val adSize: AdSize
+        get() {
+            val display = windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+
+            val density = outMetrics.density
+
+            var adWidthPixels = vbinding.adViewContainer.width.toFloat()
+            if (adWidthPixels == 0f) {
+                adWidthPixels = outMetrics.widthPixels.toFloat()
+            }
+
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +69,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     }
 
-    // Called when leaving the activity
+    /** Called when leaving the activity  */
     public override fun onPause() {
         //Sensor
         sensor_manager.unregisterListener(this)
@@ -56,7 +79,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     }
 
-    // Called when returning to the activity
+    /** Called when returning to the activity  */
     public override fun onResume() {
         super.onResume()
         //Sensor
@@ -67,7 +90,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         ad_view.resume()
     }
 
-    // Called before the activity is destroyed
+    /** Called before the activity is destroyed  */
     public override fun onDestroy() {
         //Google AdMob
         ad_view.destroy()
@@ -145,19 +168,36 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             RequestConfiguration.Builder()
                 .setMaxAdContentRating("G")
                 .setTagForChildDirectedTreatment(RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE)
+                //.setTestDeviceIds(Arrays.asList("CED7603B83B3F277F3B7BABF77EE2275")) //for test
                 .build()
         )
+
+        ad_view = AdView(this)
+        vbinding.adViewContainer.addView(ad_view)
+        // Since we're loading the banner based on the adContainerView size, we need to wait until this
+        // view is laid out before we can get the width.
+        vbinding.adViewContainer.viewTreeObserver.addOnGlobalLayoutListener {
+            if (!initialLayoutComplete) {
+                initialLayoutComplete = true
+                loadBanner()
+            }
+        }
+    }
+
+    private fun loadBanner() {
+        ad_view.adUnitId = AD_UNIT_ID
+
+        ad_view.adSize = adSize
 
         // Create an ad request.
         val adRequest = AdRequest.Builder().build()
 
-        // Start loading the ad in the background.
-        ad_view = vbinding.adView
-        ad_view.loadAd(adRequest)
 
         ad_view.adListener = object : AdListener() {
             override fun onAdLoaded() {
                 // Code to be executed when an ad finishes loading.
+                Log.d("myLog", "Ad Loaded!")
+                Toast.makeText(applicationContext, "Ad Loaded!", Toast.LENGTH_LONG)
             }
 
             override fun onAdFailedToLoad(adError: LoadAdError) {
@@ -168,18 +208,27 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             override fun onAdOpened() {
                 // Code to be executed when an ad opens an overlay that
                 // covers the screen.
+                Log.d("myLog", "Ad Opened!")
             }
 
             override fun onAdClicked() {
                 // Code to be executed when the user clicks on an ad.
+                Log.d("myLog", "Ad Clicked!")
             }
 
             override fun onAdClosed() {
                 // Code to be executed when the user is about to return
                 // to the app after tapping on an ad.
+                Log.d("myLog", "Ad Closed!")
             }
         }
-
-
+        // Start loading the ad in the background.
+        ad_view.loadAd(adRequest)
     }
+
+    companion object {
+        // This is an ad unit ID for a test ad. Replace with your own banner ad unit ID.
+        private val AD_UNIT_ID = "ca-app-pub-6561678831605592/3009074662"
+    }
+
 }
