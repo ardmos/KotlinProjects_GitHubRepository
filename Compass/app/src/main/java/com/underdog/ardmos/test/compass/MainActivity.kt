cea0087736480d22b9890e183b808fbb25c46098
ardmos <46891528+ardmos@users.noreply.github.com>
2021-12-24 20:20:16 +0900

    Kakao AdFit 배너 광고 적용 완료

--- Compass/app/src/main/java/com/underdog/ardmos/test/compass/MainActivity.kt
+++ Compass/app/src/main/java/com/underdog/ardmos/test/compass/MainActivity.kt
@@ -13,6 +13,11 @@ import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.kakao.adfit.ads.AdListener
+import com.kakao.adfit.ads.ba.BannerAdView
 import com.underdog.ardmos.test.compass.databinding.ActivityMainBinding
 import kotlin.math.round
 
@@ -32,8 +37,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     var angle : Double = 0.0
     var direction : String = ""
 
-    //Kakao Adfit
-    //val adView: BannerAdView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -71,6 +75,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         //window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         //vbinding.mainActivityLayout.setPadding(0, this.resources.getDimensionPixelSize(this.resources.getIdentifier("status_bar_height", "dimen", "android")), 0, 0)
 
+        //Kakao Adfit
+        kakaoAdFit()
 
     }
 
@@ -163,6 +169,50 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         // Be sure to unregister the sensor when the activity pauses.
         super.onPause()
         sensor_manager.unregisterListener(this)
+    }
+
+    private fun kakaoAdFit(){
+        val adView: BannerAdView = vbinding.bannerAdView
+        adView.setClientId("input-your-clientId")  // 할당 받은 광고단위 ID 설정
+        adView.setAdListener(object : AdListener {  // optional :: 광고 수신 리스너 설정
+
+            override fun onAdLoaded() {
+                // 배너 광고 노출 완료 시 호출
+            }
+
+            override fun onAdFailed(errorCode: Int) {
+                // 배너 광고 노출 실패 시 호출
+            }
+
+            override fun onAdClicked() {
+                // 배너 광고 클릭 시 호출
+            }
+
+        })
+
+        // activity 또는 fragment의 lifecycle에 따라 호출
+        lifecycle.addObserver(object : LifecycleObserver {
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+            fun onResume() {
+                adView.resume()
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+            fun onPause() {
+                adView.pause()
+            }
+
+            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            fun onDestroy() {
+                adView.destroy()
+            }
+
+        })
+
+        adView.loadAd()  // 광고 요청
+
+
     }
 }
 
