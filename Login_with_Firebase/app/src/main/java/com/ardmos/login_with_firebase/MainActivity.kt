332d8463a26c574a0b3dda1ce4f0fa16b8f4a9ea
ardmos <46891528+ardmos@users.noreply.github.com>
2022-01-29 20:21:00 +0900

    .

--- Login_with_Firebase/app/src/main/java/com/ardmos/login_with_firebase/MainActivity.kt
+++ Login_with_Firebase/app/src/main/java/com/ardmos/login_with_firebase/MainActivity.kt
@@ -2,10 +2,58 @@ package com.ardmos.login_with_firebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.ardmos.login_with_firebase.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+// Login with Firebase by Email & Password
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    val vbinding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(vbinding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        // Set OnListener on buttons
+        // button_login
+        vbinding.buttonLogin.setOnClickListener {  }
+        // button_join
+        vbinding.buttonJoin.setOnClickListener {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+        }
     }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+
 }
