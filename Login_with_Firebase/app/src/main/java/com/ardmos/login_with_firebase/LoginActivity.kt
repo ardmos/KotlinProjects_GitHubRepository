51f1e752f94dbc5bd13690547af24d06eb40c6b3
ardmos <46891528+ardmos@users.noreply.github.com>
2022-03-23 22:55:08 +0900

    .

--- Login_with_Firebase/app/src/main/java/com/ardmos/login_with_firebase/LoginActivity.kt
+++ Login_with_Firebase/app/src/main/java/com/ardmos/login_with_firebase/LoginActivity.kt
@@ -101,8 +101,8 @@ class LoginActivity : AppCompatActivity() {
                             val user = auth.currentUser
                             updateUI(user)
                             // move activity
-                            //val intent = Intent(this, MainActivity::class.java)
-                            //startActivity(intent)
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.exception)
@@ -161,6 +161,8 @@ class LoginActivity : AppCompatActivity() {
                     Log.d(TAG, "signInWithCredential:success")
                     val user = auth.currentUser
                     updateUI(user)
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
                 } else {
                     // If sign in fails, display a message to the user.
                     Log.w(TAG, "signInWithCredential:failure", task.exception)
